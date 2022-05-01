1d0ab1b1e6e7075b41653392c13a887994cd985e
Ellis Clarke <ellisbclarke03@gmail.com>
2022-05-01 18:00:47 +0100

    Commented out code
    I commended out code to allow me to focus on the JSON import without errors on the graphics side for now

--- src/main.py
+++ src/main.py
@@ -1,38 +1,44 @@
 import json
-from turtle import *
+with open('src\solato_export.json') as json_file:
+    planets = json.load(json_file)
+    print("debug mode")
+    print(planets)
 
-'[Python To JSON to come 👀]'
-planetLength = len(planets)
-distanceScale = 200
+    print(type(planets))
+    
+# from turtle import *
 
-screen = Screen()
-screen.tracer(0)
-turtle = Turtle()
-turtle.setheading(90)
+# planetLength = len(planets)
+# distanceScale = 200
 
-
-def drawPlanet(t,p):
-    t.fillcolor(p[3])
-    t.forward(p[1] * distanceScale)
-    t.begin_fill()
-    t.pendown()
-    t.circle(p[2])
-    t.penup()
-    t.goto(0, 0)
-    t.end_fill()
+# screen = Screen()
+# screen.tracer(0)
+# turtle = Turtle()
+# turtle.setheading(90)
 
 
+# def drawPlanet(t,p):
+#     t.fillcolor(p[3])
+#     t.forward(p[1] * distanceScale)
+#     t.begin_fill()
+#     t.pendown()
+#     t.circle(p[2])
+#     t.penup()
+#     t.goto(0, 0)
+#     t.end_fill()
 
-def draw():
-    turtle.clear()
-    i = 0
-    while i < planetLength:
-        drawPlanet(turtle, planets[i])
-        i += 1
-    screen.update()
-    turtle.left(1)
-    screen.ontimer(draw, 200)
 
-draw()
 
-screen.mainloop()
+# def draw():
+#     turtle.clear()
+#     i = 0
+#     while i < planetLength:
+#         drawPlanet(turtle, planets[i])
+#         i += 1
+#     screen.update()
+#     turtle.left(1)
+#     screen.ontimer(draw, 200)
+
+# draw()
+
+# screen.mainloop()

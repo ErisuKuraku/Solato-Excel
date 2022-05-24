7b0fa0eecc8d7258d856d1a32afacbe822803546
Ellis Clarke <25765196+ErisuKuraku@users.noreply.github.com>
2022-05-24 15:57:36 +0100

    Update on the miracle
    The turtle code is now un commented however, for some reason wont draw anything based on the perms i've given it. This is to be looked at in the future.

--- src/main.py
+++ src/main.py
@@ -11,42 +11,40 @@ for planet in planets:
     print(planet['type'])
 
     print(planet['size'], planet['radius'], planet['colour'])
-    
-# from turtle import *
-
-# planetLength = len(planets)
-# distanceScale = 200
-
-# screen = Screen()
-# screen.tracer(0)
-# turtle = Turtle()
-# turtle.setheading(90)
-
-
-#def function(distance, radius, colour)
-
-# def drawPlanet(t,p):
-#     t.fillcolor(p)
-#     t.forward(p[1] * distanceScale)
-#     t.begin_fill()
-#     t.pendown()
-#     t.circle(p[2])
-#     t.penup()
-#     t.goto(0, 0)
-#     t.end_fill()
 
 
 
-# def draw():
-#     turtle.clear()
-#     i = 0
-#     while i < planetLength:
-#         drawPlanet(turtle, planets[i])
-#         i += 1
-#     screen.update()
-#     turtle.left(1)
-#     screen.ontimer(draw, 200)
+from turtle import *
+planetLength = len(planets)
+distanceScale = 200
 
-# draw()
+screen = Screen()
+screen.tracer(0)
+turtle = Turtle()
+turtle.setheading(90)
 
-# screen.mainloop()
+def drawPlanet(t,p):
+     t.fillcolor(p['colour'])
+     t.forward(p['size'] * distanceScale)
+     t.begin_fill()
+     t.pendown()
+     t.circle(p['radius'])
+     t.penup()
+     t.goto(0, 0)
+     t.end_fill()
+
+
+
+def draw():
+    turtle.clear()
+    i = 0
+    while i < len(planets):
+         drawPlanet(turtle, planet[i])
+         i += 1
+         screen.update()
+         turtle.left(1)
+         screen.ontimer(draw, 200)
+
+    draw()
+
+    screen.mainloop()

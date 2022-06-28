da44446aa8eacf33c1984829d16d764875ac9c0b
Ellis Clarke <25765196+ErisuKuraku@users.noreply.github.com>
2022-06-28 15:12:25 +0100

    Optimisation
    Instead of turtle being called upon globally in some places and not in others. One universal turtle call (t) is being used to optimise and simplify the code for others and the size is multiplied by 20 to give the planets a bigger appearance on screen

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from turtle import *
 with open('solato_export.json') as json_file:
     planets = json.load(json_file)
     print("debug mode")
-    print(planets)
+    #print(planets)
 
-    print(type(planets))
-    print(type(planets[0]))
+    #print(type(planets))
+    #print(type(planets[0]))
 
 for planet in planets:
     print(planet['type'])
@@ -20,15 +20,15 @@ distanceScale = 200
 
 screen = Screen()
 screen.tracer(0)
-turtle = Turtle()
-turtle.setheading(90)
+t = Turtle()
+t.setheading(90)
 
-def drawPlanet(t,p):
+def drawPlanet(p):
     t.fillcolor(p['colour'])
     t.forward(p['radius'] * distanceScale)
     t.begin_fill()
     t.pendown()
-    t.circle(p['size'])
+    t.circle(p['size']*20)
     t.penup()
     t.goto(0, 0)
     t.end_fill()
@@ -36,13 +36,13 @@ def drawPlanet(t,p):
 
 
 def draw():
-    turtle.clear()
+    t.clear()
     i = 0 
     while i < 6:
-        drawPlanet(turtle, planets[i])
+        drawPlanet(planets[i])
         i=i+1
         screen.update()
-        turtle.left(1)
+        t.left(1)
     screen.ontimer(draw, 200)
 
 draw()
